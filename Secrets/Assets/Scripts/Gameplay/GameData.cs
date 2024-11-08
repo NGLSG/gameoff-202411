626426d9e5666a08975ee58a5ce84763019fd39d
jimmy_breyce-admin <jbreyce@gmail.com>
2024-11-08 23:02:49 +0800

    增加全局游戏数据类

---
+++ Secrets/Assets/Scripts/Gameplay/GameData.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public int exploreScore; // 探索值
+    public int answerScore; // 任务回复后的积分
+
+    /// <summary>
+    /// 任务积分变化
+    /// </summary>
+    /// <param name="changeAmount"> 任务积分变化量 </param>
+    public void ChangerAnswerScore(int changeAmount)
+    {
+        answerScore += changeAmount;
+    }
+
+    /// <summary>
+    /// 探索值变化
+    /// </summary>
+    /// <param name="changeAmount">探索值积分变化量</param>
+    public void ChangeSearchScore(int changeAmount)
+    {
+        exploreScore += changeAmount;
+    }
+}
